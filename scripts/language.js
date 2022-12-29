f9e474d1cc4df11401a31339fabf522e4f778ff9
Danique Lammertink <danique.lammertink@hva.nl>
2022-12-29 08:54:02 +0100

    ✅ Dutch language is working

--- scripts/language.js
+++ scripts/language.js
@@ -1,10 +1,11 @@
 var engelsButton = document.querySelector(".engels")
 var portugeesButton = document.querySelector(".portugees")
+var nederlandsButton = document.querySelector(".nederlands")
 var theLanguage = document.getElementsByTagName("html")[0].getAttribute("lang");
 var html = document.querySelector("html");
 
 function Engelsfunctie(){
-    if(theLanguage == "pt"){
+    if(theLanguage == "pt" || theLanguage == "nl"){
         html.setAttribute("lang", "en")
     }
     else{
@@ -14,8 +15,15 @@ function Engelsfunctie(){
 engelsButton.addEventListener("click", Engelsfunctie)
 
 function Portugeesfunctie(){
-    if(theLanguage == "en"){
+    if(theLanguage == "en" || theLanguage == "nl"){
         html.setAttribute("lang", "pt")
     }
 }
 portugeesButton.addEventListener("click", Portugeesfunctie)
+
+function Nederlandsfunctie(){
+    if(theLanguage == "pt" || theLanguage == "en"){
+        html.setAttribute("lang", "nl")
+    }
+}
+nederlandsButton.addEventListener("click", Nederlandsfunctie)
