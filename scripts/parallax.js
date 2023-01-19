9c0f39e0bf3cf87dee02691c033ca798727a9c37
Danique Lammertink <danique.lammertink@hva.nl>
2023-01-19 10:51:25 +0100

    ✅ Added orange world

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -6,19 +6,40 @@ let layer5 = document.getElementById('greenlayer5');
 let layer6 = document.getElementById('greenlayer6');
 let layer7 = document.getElementById('greenlayer7');
 let layer8 = document.getElementById('greenlayer8');
-let button1 = document.querySelector('.info1');
+let button1 = document.getElementById('info1');
+let titel = document.getElementById("titel");
+
+let olayer1 = document.getElementById("orangeworldlayer1");
+let olayer2 = document.getElementById("orangeworldlayer2");
+let olayer3 = document.getElementById("orangeworldlayer3");
+let olayer4 = document.getElementById("orangeworldlayer4");
+let olayer5 = document.getElementById("orangeworldlayer5");
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
+
     layer1.style.top = value * 1.8 + 'px';
-    layer2.style.top = value * 1.5 + 'px';
-    // layer3.style.top = value * 1.4 + 'px';
-    // layer4.style.top = value * 1.8 + 'px';
-    // layer5.style.top = value * 1.8 + 'px';
-    layer6.style.top = value * 1.5 + 'px';
-    layer7.style.top = value * 1.5 + 'px';
     button1.style.top = value * 1.5 + 'px';
+    titel.style.top = value * -1.5 + 'px';
+    layer2.style.top = value * 1.5 + 'px';
+    layer7.style.top = value * 1.5 + 'px';
 })
+
+window.addEventListener('scroll', () => {
+    let currentScroll = window.pageYOffset;
+    let startSection = document.querySelector("#orangeworld").offsetTop;
+
+    window.onscroll = function() {
+        currentScroll = window.pageYOffset;
+      }
+
+    if(currentScroll > startSection){
+        olayer2.style.transform = "translateY(" + (currentScroll - startSection) * 1.3 + "px)";
+        olayer4.style.transform = "translateY(" + (currentScroll - startSection) * 1.8 + "px)";
+    }
+})
+
+
 
 var index = 0;
 var textnl = 'Wil jij ook 0% belasting betalen? Scroll dan snel verder!';
@@ -53,3 +74,37 @@ function textEffect() {
     }
 }
 button1.addEventListener("click", textEffect)
+
+var index2 = 0;
+var text2nl = 'Wil jij weten welke landen een slecht verdrag hebben met Mozambique? Scroll dan snel verder!';
+var text2en = 'Do you also want to know which countries have a bad treaty with Mozambique? Then quickly scroll on!';
+var text2pt = "Você também quer saber quais países têm um tratado ruim com Moçambique? Em seguida, role rapidamente!";
+var speed = 50;
+  
+function text2Effect() {
+    if(document.getElementsByTagName("html")[0].getAttribute("lang") == "en"){
+        if (index2 < text2en.length) {
+            document.getElementById("kaarttekst")
+                    .innerHTML += text2en.charAt(index2);
+            index2++;
+            setTimeout(text2Effect, speed);
+        }
+    }
+    if(document.getElementsByTagName("html")[0].getAttribute("lang") == "nl"){
+        if (index2 < text2nl.length) {
+            document.getElementById("kaarttekst")
+                    .innerHTML += text2nl.charAt(index2);
+            index2++;
+            setTimeout(text2Effect, speed);
+        }
+    }
+    else{
+        if (index2 < text2pt.length) {
+            document.getElementById("kaarttekst")
+                    .innerHTML += text2pt.charAt(index2);
+            index2++;
+            setTimeout(text2Effect, speed);
+        }
+    }
+}
+olayer3.addEventListener("click", text2Effect)
