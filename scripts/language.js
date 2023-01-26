00383ab9139426862a381d957f2b7b628750e710
Danique Lammertink <danique.lammertink@hva.nl>
2023-01-26 09:45:46 +0100

    ✨ Deploy

--- scripts/language.js
+++ scripts/language.js
@@ -3,10 +3,14 @@ var portugeesButton = document.querySelector(".portugees")
 var nederlandsButton = document.querySelector(".nederlands")
 var theLanguage = document.getElementsByTagName("html")[0].getAttribute("lang");
 var html = document.querySelector("html");
+const tableofcontents = document.querySelector('.tocnl');
+const tableofcontentspt = document.querySelector('.tocpt');
+const tableofcontentsen = document.querySelector('.tocen');
 
 function Engelsfunctie(){
     if(theLanguage == "pt" || theLanguage == "nl"){
         html.setAttribute("lang", "en")
+        tableofcontentsen.style.backgroundImage = "url(./images/verdrag/blanco-verdrag-engels.png)"
     }
 }
 engelsButton.addEventListener("click", Engelsfunctie)
@@ -14,6 +18,7 @@ engelsButton.addEventListener("click", Engelsfunctie)
 function Portugeesfunctie(){
     if(theLanguage == "en" || theLanguage == "nl"){
         html.setAttribute("lang", "pt")
+        tableofcontentspt.style.backgroundImage = "url(./images/verdrag/blanco-verdrag-spaans.png)"
     }
 }
 portugeesButton.addEventListener("click", Portugeesfunctie)
@@ -21,9 +26,11 @@ portugeesButton.addEventListener("click", Portugeesfunctie)
 function Nederlandsfunctie(){
     if(theLanguage == "pt" || theLanguage == "en"){
         html.setAttribute("lang", "nl")
+        tableofcontents.style.backgroundImage = "url(../images/Verdrag-noblack.png)"
     }
     else{
         html.setAttribute("lang", "nl")
+        tableofcontents.style.backgroundImage = "url(../images/Verdrag-noblack.png)"
     }
 }
 nederlandsButton.addEventListener("click", Nederlandsfunctie)
